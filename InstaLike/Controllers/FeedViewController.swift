0d58f548f6550a0cbd7e669166601bd4364f9f41
Prashant Ghimire <prashant.ghimire@selu.edu>
2017-08-20 01:06:21 -0500

    tabor click listeners added

---
+++ InstaLike/Controllers/FeedViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FeedViewController.swift
+//  InstaLike
+//
+//  Created by Prashant Ghimire on 8/20/17.
+//  Copyright © 2017 Prashant Ghimire. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UITabBarController {
+
+    @IBAction func onLogout(_ sender: UIBarButtonItem) {
+        print("on logout click!")
+    }
+    
+    @IBAction func onCamera(_ sender: UIBarButtonItem) {
+        print("on camera")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
