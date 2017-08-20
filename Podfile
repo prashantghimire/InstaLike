b9009e216ccbc631a8ca500e2e24f7416bbd3131
Prashant Ghimire <prashant.ghimire@selu.edu>
2017-08-19 20:21:45 -0500

    added pod file

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '10.3'
+
+target 'InstaLike' do
+  use_frameworks!
+  pod 'Parse'
+end
---
+++ Pods/Target Support Files/Pods-InstaLike/Pods-InstaLike-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InstaLike : NSObject
+@end
+@implementation PodsDummy_Pods_InstaLike
+@end
